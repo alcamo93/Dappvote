10d203dcb53fc851ce4f385e45469e7a8d7be5c1
alcamo <alcamo93@gmail.com>
2020-09-03 05:42:04 -0400

    deploy

---
+++ precache-manifest.81b450bf62592a68337a783e3a082fae.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7ad3b48db73dd7a4018021d21b05ec1",
+    "url": "/Dappvote/img/dvote 54x54.d7ad3b48.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Dappvote/robots.txt"
+  },
+  {
+    "revision": "1b0ebcf31a849a62700e682c72de49f1",
+    "url": "/Dappvote/index.html"
+  },
+  {
+    "revision": "c33164f20ca101dc8bf8",
+    "url": "/Dappvote/css/app.59873a48.css"
+  },
+  {
+    "revision": "9ac8595a8c27752e73ed",
+    "url": "/Dappvote/css/chunk-vendors.96d1351d.css"
+  },
+  {
+    "revision": "9ac8595a8c27752e73ed",
+    "url": "/Dappvote/js/chunk-vendors.1ab4ad21.js"
+  },
+  {
+    "revision": "462aa5f39cbb8f3559650c7d36041506",
+    "url": "/Dappvote/img/Fintech.462aa5f3.jpg"
+  },
+  {
+    "revision": "755e6b801c8099671aee8aaf6bb96b4b",
+    "url": "/Dappvote/img/Cover votacion.755e6b80.png"
+  },
+  {
+    "revision": "e4e4e70b96cfaca6c1cada80922cea6d",
+    "url": "/Dappvote/img/escuela 54x54.e4e4e70b.png"
+  },
+  {
+    "revision": "eebfe159425ef4e4ee92",
+    "url": "/Dappvote/js/about.d12872ee.js"
+  },
+  {
+    "revision": "c33164f20ca101dc8bf8",
+    "url": "/Dappvote/js/app.ab6eda98.js"
+  },
+  {
+    "revision": "fe4792d482196a50cf9ae0d9d90b6493",
+    "url": "/Dappvote/faviconvue.ico"
+  },
+  {
+    "revision": "755e6b801c8099671aee8aaf6bb96b4b",
+    "url": "/Dappvote/img/Cover votacion.png"
+  },
+  {
+    "revision": "462aa5f39cbb8f3559650c7d36041506",
+    "url": "/Dappvote/img/Fintech.jpg"
+  },
+  {
+    "revision": "05ebfa0dcf8120fbd6ec181e8414dbf2",
+    "url": "/Dappvote/img/dvote 16x16.png"
+  },
+  {
+    "revision": "d7ad3b48db73dd7a4018021d21b05ec1",
+    "url": "/Dappvote/img/dvote 54x54.png"
+  },
+  {
+    "revision": "e4e4e70b96cfaca6c1cada80922cea6d",
+    "url": "/Dappvote/img/escuela 54x54.png"
+  },
+  {
+    "revision": "eebfe159425ef4e4ee92",
+    "url": "/Dappvote/css/about.27d9d8a1.css"
+  }
+];
